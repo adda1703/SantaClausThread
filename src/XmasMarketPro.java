e1c0109c57b56c274b03b5ed90ea8d8ff8999e1e
adda1703 <120007386+adda1703@users.noreply.github.com>
2023-12-01 19:24:52 +0100

    Create XmasMarketPro.java

---
+++ src/XmasMarketPro.java
@@ -0,0 +1,25 @@
+
+import java.util.Random;
+//library for random function
+
+public class XmasMarketPro {
+
+		private final static int N_SC = 4;
+		//numbers of Santa Claus in the market
+		
+		public static void main(String[] args) {
+			
+		Random r=new Random(System.currentTimeMillis());
+		
+		int client = r.nextInt(N_SC);
+		
+		Thread[] new_clients =new Thread[client];
+		
+		for(int i=0; i<N_SC; i++) {
+			boolean 
+			
+			
+		}
+			
+		}
+}
