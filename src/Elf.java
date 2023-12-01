ea3ee43ed408f1838a1759f0d139f578d64c5db9
adda1703 <120007386+adda1703@users.noreply.github.com>
2023-12-01 20:35:05 +0100

    finish part 1

---
+++ src/Elf.java
@@ -0,0 +1,16 @@
+
+public class Elf extends Thread{
+	
+	private String colour;
+	
+	public Elf(String colour) {
+		this.colour=colour;
+	}
+	
+	@Override
+	public void run() {
+		System.out.println("The " + this.colour+ " Elf starts singing");
+		System.out.println("The " + this.colour +" Elf brings more gifts");
+		System.out.println("The " + this.colour + " Elf finishes is work");
+	}
+}
--- src/XmasMarketPro.java
+++ src/XmasMarketPro.java
@@ -6,20 +6,66 @@ public class XmasMarketPro {
 
 		private final static int N_SC = 4;
 		//numbers of Santa Claus in the market
+		private final static int N_E= 5;
+		//number of max elfs
+		private final static String[] NAMES = {"Elena", "Sergio", "Adelina", "Alessandra",
+				"Mario", "Sofia"};
+		//scrivo  possibili nomi per i clienti
+		private final static String[] COLOURS= {"red", "green", "yellow", "pink", "violet", "blue"};
+		//scrivo possibili colori per gli elfi
 		
 		public static void main(String[] args) {
 			
 		Random r=new Random(System.currentTimeMillis());
 		
-		int client = r.nextInt(N_SC);
+		int n_children = r.nextInt(N_SC);
+		int n_elfs=r.nextInt(N_E);
 		
-		Thread[] new_clients =new Thread[client];
+		Thread[] threads_to_start =new Thread[n_children+n_elfs];
 		
-		for(int i=0; i<N_SC; i++) {
-			boolean 
+		for(int i=0; i<n_children; i++) {
+			
+			boolean nice= r.nextBoolean(); //scelgo in modo random se il bambino è nice o bad
+			
+			int indexName= r.nextInt(NAMES.length);
+			
+			if(nice) {
+				NiceChild c = new NiceChild(NAMES[indexName]);
+				threads_to_start[i]= new Thread (c);
+			}	else {
+				BadChild c =new BadChild (NAMES[indexName]);
+				threads_to_start[i]= new Thread (c);}	
+				
+			}
+		
+		for(int i=0; i<n_elfs; i++) {
 			
 			
+			int indexName= r.nextInt(COLOURS.length);
+			
+			Elf e = new Elf(COLOURS[indexName]);
+				threads_to_start[n_children+i]= e;
+		
 		}
+		
+		//faccio partire i thread
+		for(Thread t : threads_to_start) {
+			t.start();
+		}
+		
+		//exception
+		for(Thread t: threads_to_start) {
+			try {
+				t.join();
+			} catch (InterruptedException e) {
+				System.err.println("Main Thread has been interrupted");
+				e.printStackTrace();
+				}
+		}
+		
+		System.out.println("Market closed");
+		
+		
 			
 		}
 }
